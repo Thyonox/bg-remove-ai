c6f3ecce97c5392c04341582eb33990e02221e11
thyonix <dongf7889@gmail.com>
2025-08-12 10:18:09 +0800

    Navbar、Header、Steps、Bgslider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import BuyCredit from './pages/BuyCredit';
+import Navbar from './components/Navbar';
 
 const App = () => {
 
   return (
-    <div className='text-9xl'>
-      div
+    <div className='min-h-screen bg-slate-50'>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/result' element={<Result />} />
+          <Route path='/buy' element={<BuyCredit />} />
+        </Routes>
     </div>
   );
 };
